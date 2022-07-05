d9da8ea1e4ae1060711a05e24187967f092dbb1a
Даниил Камский <toolumini@mail.ru>
2022-07-05 16:44:55 +0300

    littlefixДобавьте файлы проекта.

---
+++ 21 game/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿
+namespace _21_game
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Card1 = new System.Windows.Forms.Panel();
+            this.Card1Text = new System.Windows.Forms.Label();
+            this.Card2 = new System.Windows.Forms.Panel();
+            this.Card2Text = new System.Windows.Forms.Label();
+            this.Card3 = new System.Windows.Forms.Panel();
+            this.Card3Text = new System.Windows.Forms.Label();
+            this.Card4 = new System.Windows.Forms.Panel();
+            this.Card4Text = new System.Windows.Forms.Label();
+            this.Card5 = new System.Windows.Forms.Panel();
+            this.Card5Text = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Card1.SuspendLayout();
+            this.Card2.SuspendLayout();
+            this.Card3.SuspendLayout();
+            this.Card4.SuspendLayout();
+            this.Card5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Card1
+            // 
+            this.Card1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.Card1.Controls.Add(this.Card1Text);
+            this.Card1.Location = new System.Drawing.Point(127, 313);
+            this.Card1.Name = "Card1";
+            this.Card1.Size = new System.Drawing.Size(91, 125);
+            this.Card1.TabIndex = 0;
+            // 
+            // Card1Text
+            // 
+            this.Card1Text.AutoSize = true;
+            this.Card1Text.Location = new System.Drawing.Point(19, 55);
+            this.Card1Text.Name = "Card1Text";
+            this.Card1Text.Size = new System.Drawing.Size(50, 20);
+            this.Card1Text.TabIndex = 0;
+            this.Card1Text.Text = "label1";
+            // 
+            // Card2
+            // 
+            this.Card2.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.Card2.Controls.Add(this.Card2Text);
+            this.Card2.Location = new System.Drawing.Point(224, 313);
+            this.Card2.Name = "Card2";
+            this.Card2.Size = new System.Drawing.Size(91, 125);
+            this.Card2.TabIndex = 1;
+            // 
+            // Card2Text
+            // 
+            this.Card2Text.AutoSize = true;
+            this.Card2Text.Location = new System.Drawing.Point(19, 55);
+            this.Card2Text.Name = "Card2Text";
+            this.Card2Text.Size = new System.Drawing.Size(50, 20);
+            this.Card2Text.TabIndex = 1;
+            this.Card2Text.Text = "label2";
+            // 
+            // Card3
+            // 
+            this.Card3.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.Card3.Controls.Add(this.Card3Text);
+            this.Card3.Location = new System.Drawing.Point(321, 313);
+            this.Card3.Name = "Card3";
+            this.Card3.Size = new System.Drawing.Size(91, 125);
+            this.Card3.TabIndex = 1;
+            // 
+            // Card3Text
+            // 
+            this.Card3Text.AutoSize = true;
+            this.Card3Text.Location = new System.Drawing.Point(19, 55);
+            this.Card3Text.Name = "Card3Text";
+            this.Card3Text.Size = new System.Drawing.Size(50, 20);
+            this.Card3Text.TabIndex = 2;
+            this.Card3Text.Text = "label3";
+            // 
+            // Card4
+            // 
+            this.Card4.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.Card4.Controls.Add(this.Card4Text);
+            this.Card4.Location = new System.Drawing.Point(418, 313);
+            this.Card4.Name = "Card4";
+            this.Card4.Size = new System.Drawing.Size(91, 125);
+            this.Card4.TabIndex = 1;
+            // 
+            // Card4Text
+            // 
+            this.Card4Text.AutoSize = true;
+            this.Card4Text.Location = new System.Drawing.Point(20, 55);
+            this.Card4Text.Name = "Card4Text";
+            this.Card4Text.Size = new System.Drawing.Size(50, 20);
+            this.Card4Text.TabIndex = 3;
+            this.Card4Text.Text = "label4";
+            // 
+            // Card5
+            // 
+            this.Card5.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.Card5.Controls.Add(this.Card5Text);
+            this.Card5.Location = new System.Drawing.Point(515, 313);
+            this.Card5.Name = "Card5";
+            this.Card5.Size = new System.Drawing.Size(91, 125);
+            this.Card5.TabIndex = 1;
+            // 
+            // Card5Text
+            // 
+            this.Card5Text.AutoSize = true;
+            this.Card5Text.Location = new System.Drawing.Point(18, 55);
+            this.Card5Text.Name = "Card5Text";
+            this.Card5Text.Size = new System.Drawing.Size(50, 20);
+            this.Card5Text.TabIndex = 4;
+            this.Card5Text.Text = "label5";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(612, 313);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(176, 62);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Взять ещё";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(612, 381);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(176, 57);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Спасовать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 368);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(612, 13);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(176, 71);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Начать/Перезапустить игру";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(117, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(466, 27);
+            this.textBox1.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Card5);
+            this.Controls.Add(this.Card4);
+            this.Controls.Add(this.Card3);
+            this.Controls.Add(this.Card2);
+            this.Controls.Add(this.Card1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Card1.ResumeLayout(false);
+            this.Card1.PerformLayout();
+            this.Card2.ResumeLayout(false);
+            this.Card2.PerformLayout();
+            this.Card3.ResumeLayout(false);
+            this.Card3.PerformLayout();
+            this.Card4.ResumeLayout(false);
+            this.Card4.PerformLayout();
+            this.Card5.ResumeLayout(false);
+            this.Card5.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel Card1;
+        private System.Windows.Forms.Label Card1Text;
+        private System.Windows.Forms.Panel Card2;
+        private System.Windows.Forms.Label Card2Text;
+        private System.Windows.Forms.Panel Card3;
+        private System.Windows.Forms.Label Card3Text;
+        private System.Windows.Forms.Panel Card4;
+        private System.Windows.Forms.Label Card4Text;
+        private System.Windows.Forms.Panel Card5;
+        private System.Windows.Forms.Label Card5Text;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ 21 game/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _21_game
+{
+    public partial class Form1 : Form
+    {
+        int cardcount=-1;
+        static List<int> Startcards = new List<int>(){ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
+        static List<int> cards = new List<int>(Startcards);
+        private int playercount=0;
+        private int botcount=0;
+        bool playerpass, botpass;
+        Random r = new Random();
+        Label [] cardds = new Label[5];
+        public Form1()
+        {
+            InitializeComponent();
+            cardcount = -1;
+            
+            cardds[0] = Card1Text;
+            cardds[1] = Card2Text;
+            cardds[2] = Card3Text;
+            cardds[3] = Card4Text;
+            cardds[4] = Card5Text;
+            Card1Text.Text = "";
+            Card2Text.Text = "";
+            Card3Text.Text = "";
+            Card4Text.Text = "";
+            Card5Text.Text = "";
+            label1.Text = "";
+            label2.Text = "";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Card1Text.Text = "";
+            Card2Text.Text = "";
+            Card3Text.Text = "";
+            Card4Text.Text = "";
+            Card5Text.Text = "";
+            label1.Text = "";
+            label2.Text = "";
+            label1.Text = "";
+            label2.Text = "";
+            textBox1.Text = "";
+            playercount = 0;
+            botcount = 0;
+            cardcount = -1;
+            cards.Clear();
+            playerpass = false;
+            botpass = false;
+            for(int i =1; i <= 11; i++)
+            {
+                cards.Add(i);
+            }
+        }
+        
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (botcount > 16)
+                botpass = true;
+            playerpass = true;
+            if(playerpass == true && botpass == true)
+            {
+                pass();
+            }
+            else
+            {
+                pull("bot");
+            }
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            if (playerpass == true && botpass == true)
+            {
+                pass();
+            }
+            if (playercount < 21)
+            {
+                cardcount++;
+                pull("player");
+            }
+            else
+            {
+                playerpass = true;
+            }
+            
+        }
+
+        private void pull(string who)
+        {
+            int numbufcard;
+            if (botcount > 21)
+                botpass = true;
+            if (playercount > 21)
+                playerpass = true;
+            switch (who)
+            {
+                case "player":
+                    numbufcard = r.Next(0, cards.Count - 1);
+                    playercount += cards[numbufcard];
+                    label1.Text = Convert.ToString(playercount);
+                    cardds[cardcount].Text = Convert.ToString(cards[numbufcard]);
+                    cards.RemoveAt(numbufcard);
+                    pull("bot");
+                    if (playercount > 16)
+                        playerpass = true;
+                    break;
+                case "bot":
+                    numbufcard = r.Next(0, cards.Count - 1);
+                    if (botcount <= 16  && check(cards) == true)
+                    {
+                        cards.RemoveAt(numbufcard);
+                        botcount += cards[numbufcard]-1;
+                        label2.Text = Convert.ToString(botcount);  
+                    }
+                    else
+                    {
+                        botpass = true;
+                        pass();
+                    }
+                    
+                    break;
+            }
+        }
+        private void pass()
+        {
+            if (botcount > playercount && botcount <= 21)
+            {
+                MessageBox.Show("Вы проиграли!");
+            }
+            else if (botcount < playercount && playercount <= 21)
+            {
+                MessageBox.Show("Вы выиграли!");
+            }
+            else if (botcount == playercount || playercount >21 && botcount > 21)
+            {
+                MessageBox.Show("Ничья!");
+            }
+            else if(botcount>21 && playercount <= 21)
+            {
+                MessageBox.Show("Вы выиграли!");
+            }
+            else if(botcount <= 21 && playercount > 21)
+            {
+                MessageBox.Show("Вы выиграли!");
+            }
+        }
+        private bool check(List<int> l)
+        {
+            int coltrue = 0;
+            int colfalse = 0;
+            for(int i =0; i< l.Count; i++)
+            {
+                if (l[i] > (21 - botcount))
+                {
+                    colfalse++;
+                }
+                else
+                {
+                    coltrue++;
+                }
+                
+            }
+            if (colfalse > coltrue)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+    
+}
